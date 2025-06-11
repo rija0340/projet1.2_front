648f7a4a286071b806447b12fdc7097943f5627e
Raberia <rakotoarinelinarija@gmail.com>
2025-06-11 13:52:24 +0300

    initialisation, isntallation daisyui et tailwindvite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+function App() {
+
+  return (
+    <>
+     <button className="btn btn-xs">Xsmall</button>
+<button className="btn btn-sm">Small</button>
+<button className="btn">Medium</button>
+<button className="btn btn-lg">Large</button>
+<button className="btn btn-xl">Xlarge</button>
+    </>
+  )
+}
+
+export default App
